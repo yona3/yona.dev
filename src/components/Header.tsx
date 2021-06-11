392e1d0e1c50972ce5bc0265c6aec864cf008dcb
yona3 <kou.y.0905@icloud.com>
2021-06-11 16:43:43 +0900

    🩹 implove classname readability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,12 @@ const ITEMS = ["About", "Products", "Blog"];
 
 export const Header: VFC = () => {
   return (
-    <header className="py-5 px-5 sm:px-10 font-mono text-center bg-yellow-50 shadow">
+    <header
+      className="
+        py-5 px-5 sm:px-10 font-mono
+        text-center bg-yellow-50 shadow
+      "
+    >
       <div className="flex justify-between items-center mx-auto max-w-screen-xl ">
         <Link href="/">
           <h1 className="text-base sm:text-xl text-gray-800 cursor-pointer ">
@@ -19,14 +24,26 @@ export const Header: VFC = () => {
               {ITEMS.map((name) => {
                 return name === "Blog" ? (
                   <Link key="name" href="/blog">
-                    <li className="box-border relative flex-col-reverse border-b-2 border-gray-800 border-opacity-0 hover:border-opacity-100 transition duration-200 cursor-pointer">
+                    <li
+                      className="
+                        cursor-pointer
+                        box-border relative flex-col-reverse
+                        border-b-2 border-gray-800 border-opacity-0
+                        hover:border-opacity-100 transition duration-200
+                      "
+                    >
                       {name}
                     </li>
                   </Link>
                 ) : (
                   <li
                     key={name}
-                    className="box-border relative flex-col-reverse border-b-2 border-gray-800 border-opacity-0 hover:border-opacity-100 transition duration-200 cursor-pointer"
+                    className="
+                      cursor-pointer
+                      box-border relative flex-col-reverse 
+                      border-b-2 border-gray-800 border-opacity-0
+                      hover:border-opacity-100 transition duration-200
+                    "
                   >
                     {name}
                   </li>
