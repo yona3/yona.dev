aaa600780c6b5938a99f48e6fbc7dc9ebdd9c356
yona3 <kou.y.0905@icloud.com>
2021-06-17 16:23:29 +0900

    📝 Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,12 +10,12 @@ export const About = forwardRef<HTMLDivElement>((_, ref) => {
           <h2 className="text-2xl font-semibold">About</h2>
           <div className="px-4 mt-8 text-sm sm:text-base text-left">
             <p>
-              私は地方大学の理学部に所属している大学生です。
-              大学1年生のときにWeb開発に興味を持ち、勉強を始めました。
+              私は琉球大学の理学部に所属している大学生です。
+              卒業後はWebエンジニアとして就職したいのでWeb開発の勉強をしています。
             </p>
             <br />
             <p>
-              現在はReactとFirebaseで構築したWebサービスを運営したり、TypeScriptを使ったバックエンド開発の勉強をしています。
+              現在は主にReactとFirebaseで構築したWebサービスを運営・開発したり、TypeScriptを使ったAPI開発の勉強をしています。
             </p>
           </div>
         </div>
