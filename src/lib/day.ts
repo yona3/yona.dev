6d519f4eab78e22489f8715c80f492b009208ef4
yona3 <kou.y.0905@icloud.com>
2021-08-07 19:38:35 +0900

    🔨 Update date format

--- src/lib/day.ts
+++ src/lib/day.ts
@@ -1,3 +1,3 @@
 import dayjs from "dayjs";
 
-export const formatDate = (date: string) => dayjs(date).format("YYYY.MM.DD");
+export const formatDate = (date: string) => dayjs(date).format("YYYY-MM-DD");
