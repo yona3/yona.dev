151d30eaf1705d66fb4ae2929c1b2e9121e1a66d
yona3 <kou.y.0905@icloud.com>
2021-08-08 15:42:22 +0900

    🍱 Update OGP back image

--- src/lib/generageOGP.ts
+++ src/lib/generageOGP.ts
@@ -1,7 +1,7 @@
 import base64url from "base64url";
 
 const imageUrl =
-  "https://images.microcms-assets.io/assets/3c1fa25360b2455ab94fa433bfcfb496/34083129afa54933b48ec5d774a4750c/%40js_yona%20(1).png";
+  "https://images.microcms-assets.io/assets/3c1fa25360b2455ab94fa433bfcfb496/a084e72157354acea22bcace22918499/%40js_yona%20(2).png";
 
 export const generateOGPUrl = (title: string) => {
   const base64title = base64url(title);
