be813c54ff80b288c94168369c1ce130e1fa81f8
yona3 <kou.y.0905@icloud.com>
2021-08-08 14:51:26 +0900

    💄 Update OGP text style

--- src/lib/generageOGP.ts
+++ src/lib/generageOGP.ts
@@ -5,7 +5,7 @@ const imageUrl =
 
 export const generateOGPUrl = (title: string) => {
   const base64title = base64url(title);
-  const textImage = `https://images.microcms-assets.io/~text?txtsize=48&w=1000&h=400&txt-align=left,middle&txtfont=Hiragino%20Sans%20W6&txt64=${base64title}`;
+  const textImage = `https://images.microcms-assets.io/~text?txtsize=48&w=1000&h=400&txt-align=center,middle&txtfont=Hiragino%20Sans%20W6&txt64=${base64title}`;
   const textImageUrl = base64url(textImage);
   return `${imageUrl}?w=1200&mark-align=center%2Cmiddle&mark64=${textImageUrl}`;
 };
